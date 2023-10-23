a9ce567f8887cd904c02538ee475d4218af2260e
unknown <emmansairun@gmail.com>
2023-10-23 17:35:46 +0100

    creating the login page

---
+++ src/app/registrationlogin/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Registration from '../../../components/molecules/Registration'
+
+function page() {
+  return (
+    <div>
+        <Registration /> 
+    </div>
+  )
+}
+
+export default page
